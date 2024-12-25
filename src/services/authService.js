49dbb7f7c95ed1dbbbf7fab64244107672424493
angel9154 <angelgerenavazquez@outlook.com>
2024-12-25 15:16:10 -0500

    finally done

---
+++ src/services/authService.js
@@ -0,0 +1,64 @@
+// src/services/authService.js
+
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+
+const signin = async (user) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      });
+      const json = await res.json();
+  
+      if (json.error) {
+        throw new Error(json.error);
+      }
+  
+      if (json.token) {
+        localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+        return user;
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+  
+const signup = async (formData) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/users/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+    const json = await res.json();
+    if (json.err) {
+      throw new Error(json.err);
+    }
+
+       // Store the token in localStorage to keep the user signed in
+       if (json.token) {
+        localStorage.setItem('token', json.token);  // Save the token in localStorage
+      }
+
+    return json;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const getUser = () =>  {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  const user = JSON.parse(atob(token.split('.')[1]));
+  return user;
+}
+
+const signout = () => {
+    localStorage.removeItem('token');
+}
+export { signup, signin, getUser, signout };
